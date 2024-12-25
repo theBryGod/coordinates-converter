7452d54bc6d922291f9b970a1ef34db8e51e8fa1
CBA <bryan.ampit@gmail.com>
2024-12-25 21:17:13 +0800

    Add output settings layout sans functionality

--- gui-version/coordinates-converter-gui.py
+++ gui-version/coordinates-converter-gui.py
@@ -14,6 +14,12 @@ def change_input_menu_wgs84():
 def change_input_menu_prs92():
     pass
 
+def change_output_menu_wgs84():
+    pass
+
+def change_output_menu_prs92():
+    pass
+
 # app geometry
 root = ttk.Window(title="CBA's Coordinates Converter", themename="darkly")
 root.iconbitmap("converter-icon.ico")
@@ -58,6 +64,30 @@ rdbtn_input_system_prs92.pack(padx=5, pady=2, anchor="w")
 lbl_menu_input_format_desc = ttk.Label(master=frm_input_settings, text="Input coordinates format:")
 lbl_menu_input_format_desc.pack()
 mb_input_format.pack(pady=5)
+
+# output settings
+frm_output_settings = ttk.Frame(master=frm_io_settings)
+frm_output_settings.pack(side="left", padx=5)
+lblfrm_output_system = ttk.LabelFrame(master=frm_output_settings, text="Converting to:")
+lblfrm_output_system.pack(pady=5)
+var_output_system = ttk.StringVar()
+var_mb_output_format = ttk.StringVar()
+mb_output_format = ttk.Menubutton(frm_output_settings, width=14, bootstyle="secondary", textvariable=var_mb_output_format)
+rdbtn_output_system_wgs84 = ttk.Radiobutton(master=lblfrm_output_system,
+                                           text="WGS84",
+                                           variable=var_output_system,
+                                           value="wgs84",
+                                           command=change_output_menu_wgs84)
+rdbtn_output_system_wgs84.pack(padx=5, pady=(2, 0), anchor="w")
+rdbtn_output_system_prs92 = ttk.Radiobutton(master=lblfrm_output_system,
+                                           text="PRS92",
+                                           variable=var_output_system,
+                                           value="prs92",
+                                           command=change_output_menu_prs92)
+rdbtn_output_system_prs92.pack(padx=5, pady=2, anchor="w")
+lbl_menu_output_format_desc = ttk.Label(master=frm_output_settings, text="Output coordinates format:")
+lbl_menu_output_format_desc.pack()
+mb_output_format.pack(pady=5)
 
 # app mainloop
 root.mainloop()
